e88b3e64e5ee253274af951abee3ce4e31a1c17b
Gonzalo Diaz Mizzau <gonzajd777@gmail.com>
2024-03-12 00:15:45 -0300

    12/03/2024

---
+++ desafios/desafio-entregable-12/test/products.test.js
@@ -0,0 +1,6 @@
+import { CKE_SCT, CNX_STR, PORT,MODE,PERSISTENCE } from "../src/config/config.js";
+import {logger} from "../src/config/logger.config.js";
+import mongoose from "mongoose";
+import chai from "chai";
+import supertest from "supertest";
+import mocha from "mocha";
